d3893384d170c8632c317118ae44551f97eb146c
hsy110405 <hsy110405@gmail.com>
2021-02-13 13:24:10 -0800

    fix: delete admin directory

--- src/admin/controller.js
+++
@@ -1,8 +0,0 @@
-// TODO
-export const addAdmin = async(req, res) => {
-    return res.status(200).send("TODO")
-}
-
-export const getAdmin = async(req, res) => {
-    return res.status(200).send("TODO")
-}
--- src/admin/index.js
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express'
-import { addAdmin, getAdmin } from './controller.js'
-
-const router = Router()
-
-router.post('/', addAdmin)
-router.get('/:userId', getAdmin)
-
-export default router
