06d0bc84fbd8dad506d8b4a5d9de241aa080f727
Ken Utsunomiya <67528331+KenUtsunomiya@users.noreply.github.com>
2021-02-13 15:51:18 -0800

    Update src/transaction/index.js
    Co-authored-by: Hyunuk Lim <dev.hyunuk@gmail.com>

--- src/transaction/index.js
+++ src/transaction/index.js
@@ -3,7 +3,6 @@ import { addTransaction, getTransactions } from "./controller"
 
 const router = Router()
 
-router.post('/transactions', addTransaction)
 router.get('/transactions', getTransactions)
 
 export default router
