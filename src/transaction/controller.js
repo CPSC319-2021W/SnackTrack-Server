6b272043e0f77e03fe7e435ace081d62cedebfe6
Ken Utsunomiya <67528331+KenUtsunomiya@users.noreply.github.com>
2021-02-14 11:18:16 -0800

    Update src/transaction/controller.js
    Co-authored-by: Justin Wong <32123388+justincswong@users.noreply.github.com>

--- src/transaction/controller.js
+++ src/transaction/controller.js
@@ -25,7 +25,6 @@ export const addTransaction = async(req, res) => {
         const result = await Transactions.create(transaction)
 
         return res.status(201).send(result)
-
     } catch (err) {
         if (err.message === BAD_REQUEST) return res.status(400).send({ Error: "Bad Request" })
         else if (err.message === NOT_AUTHORIZED) return res.status(401).send({ Error: "Not Authorized" })
