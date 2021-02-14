176b1cfd2834cdc2d0a47afe3b72f7410de28a0d
KenUtsunomiya <ken.suruga.varuhara@gmail.com>
2021-02-13 18:04:57 -0800

    fix: Fix the status code

--- src/transaction/controller.js
+++ src/transaction/controller.js
@@ -8,7 +8,7 @@ export const getTransactions = async(req, res) => {
         const transactions =  await Transactions.findAll()
         const response = JSON.stringify(transactions)
 
-        return res.status(201).send(response)
+        return res.status(200).send(response)
 
     } catch (err) {
         return res.status(401).send({ Error: err.message })
