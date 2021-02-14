91aa27c7361345abc891d975d0e2ed7b635e383e
bzh <howardbian623@gmail.com>
2021-02-14 12:33:44 -0800

    feat: implemented GET /snacks

--- src/snack/controller.js
+++ src/snack/controller.js
@@ -5,22 +5,16 @@ const NOT_AUTHORIZED = "401"
 const NOT_FOUND = "404"
 const CONFLICT = "409"
 
-export const addSnack = async(req, res) => {
+export const getSnacks = async(req, res) => {
     try {
-        const snack = req.body
-        const result = await Snacks.create(snack)
-
-        return res.status(201).send(result)
-
+        const snacks =  await Snacks.findAll()
+        const response = JSON.stringify(snacks)
+        return res.status(200).send(response)
     } catch (err) {
-        if (err.message === BAD_REQUEST) {
-            return res.status(400).send({ Error: "Bad Request" })
-        } else if (err.message === NOT_AUTHORIZED) {
-            return res.status(401).send({ Error: "Not Authorized" })
-        } else if (err.message === CONFLICT) {
-            return res.status(409).send({ Error: "Conflict" })
+        if (err.message === NOT_FOUND) {
+            return res.status(404).send({ Error: "Not Found" })
         } else {
-            return res.status(500).send({ Error: "Internal Server Error" })
+            return res.status(400).send({ Error: err.message })
         }
     }
 }
