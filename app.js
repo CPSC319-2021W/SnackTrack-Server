4447d3ded5e107871378d6feb5ae52905647d6c5
hsy110405 <hsy110405@gmail.com>
2021-02-14 11:31:11 -0800

    setup: setup model.js and basic structure

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import userRouter from './src/user/index.js'
+import paymentRouter from './src/payment/index.js'
 import dotenv from 'dotenv'
 dotenv.config()
 
@@ -9,8 +10,6 @@ app.set('port', process.env.PORT)
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
 app.use('/users', userRouter)
+app.use('/payments', paymentRouter)
 
 export default app
-
-
-
