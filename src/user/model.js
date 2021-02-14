7e3d078a94c3b40fa4a305f3964cad9b7d163f7b
hsy110405 <hsy110405@gmail.com>
2021-02-14 15:47:13 -0800

    Revert "Revert "Merge branch 'SNAK-84' into main""
    This reverts commit d6bfe6c4494a9fcd3ebd645396ab4d2cc674d1bd.

---
+++ src/user/model.js
@@ -0,0 +1,30 @@
+import { Sequelize } from 'sequelize'
+import { db } from '../db/index.js'
+
+const DataTypes = Sequelize.DataTypes
+
+export const Users = db.define('users', {
+  user_id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  username: {
+      type: DataTypes.STRING,
+  },
+  first_name: {
+    type: DataTypes.STRING,
+  },
+  last_name: {
+    type: DataTypes.STRING
+  },
+  email_address:  {
+    type: DataTypes.STRING,
+  },
+  balance: {
+    type: DataTypes.INTEGER,
+  },
+  is_active: {
+    type: DataTypes.BOOLEAN,
+  }
+})
