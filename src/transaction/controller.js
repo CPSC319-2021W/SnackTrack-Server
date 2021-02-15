e6eec2d24c51c9277a072baaee6651d606e58f6f
KenUtsunomiya <ken.suruga.varuhara@gmail.com>
2021-02-15 03:32:22 -0800

    fix: delete addTransaction

--- src/transaction/controller.js
+++ src/transaction/controller.js
@@ -1,4 +1,4 @@
-import { Transactions } from "./model.js"
+import { Transactions } from './model.js'
 
 // Return all transactions
 export const getTransactions = async(req, res) => {
@@ -11,21 +11,3 @@ export const getTransactions = async(req, res) => {
         return res.status(401).send({ Error: err.message })
     }
 }
-
-// export const addTransaction = async(req, res) => {
-//     try {
-//         console.log("Adding a new transaction")
-//         const transaction = req.body
-//
-//         const result = await Transactions.create(transaction)
-//         console.log('Successfully added : ', result)
-//         res.sendStatus(201)
-//         // 201 Created
-//
-//         console.log("Successfully added a new transaction")
-//     } catch (err) {
-//         // 400 Bad Request
-//         // 401 Not Authorized
-//         // 409 Conflict
-//     }
-// }
