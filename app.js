9e67dd29af8629d1f9c8d6f2b0fd62381dba5ce7
KenUtsunomiya <ken.suruga.varuhara@gmail.com>
2021-02-16 14:44:17 -0800

    fix : fix Routes

--- app.js
+++ app.js
@@ -1,16 +1,15 @@
 import express from 'express'
 import dotenv from 'dotenv'
-// import cors from 'cors'
+import cors from 'cors'
 import router from './index.route.js'
 dotenv.config()
 
 const app = express()
 
 app.set('port', process.env.PORT)
-// app.use(cors())
+app.use(cors())
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
-app.use('/api/v1/', router)
-
+app.use('/api/v1', router)
 
 export default app
--- index.route.js
+++ index.route.js
@@ -6,9 +6,9 @@ import snackRouter from './src/snack/index.js'
 
 const router = Router()
 
-router.use('admins', adminRouter)
-router.use('users', userRouter)
-router.use('transactions', transactionRouter)
-router.use('snacks', snackRouter)
+router.use('/admins', adminRouter)
+router.use('/users', userRouter)
+router.use('/transactions', transactionRouter)
+router.use('/snacks', snackRouter)
 
 export default router
