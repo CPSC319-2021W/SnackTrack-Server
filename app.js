cfa00f3f4612ad87b1b5f9824d3e223a6fad0b0b
hsy110405 <hsy110405@gmail.com>
2021-02-15 20:43:43 -0800

    Revert "feat: add cors package"
    This reverts commit d45b2128bff141fc8b99aed38e858ae47881c32e.

--- app.js
+++ app.js
@@ -3,13 +3,11 @@ import userRouter from './src/user/index.js'
 import adminRouter from './src/admin/index.js'
 import transactionRouter from './src/transaction/index.js'
 import dotenv from 'dotenv'
-import cors from 'cors'
 dotenv.config()
 
 const app = express()
 
 app.set('port', process.env.PORT)
-app.use(cors())
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
 app.use('/admins', adminRouter)
