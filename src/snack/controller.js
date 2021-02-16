b17a9c998f287c67f911e7c6fc3576a4e4f6c354
bzh <howardbian623@gmail.com>
2021-02-15 17:12:30 -0800

    refactor: removed the unnecessary error code check

--- src/snack/controller.js
+++ src/snack/controller.js
@@ -1,7 +1,5 @@
 import Snacks from './model.js'
 
-const NOT_FOUND = '404'
-
 export const getSnacks = async(req, res) => {
     try {
         const snacks =  await Snacks.findAll()
@@ -9,10 +7,6 @@ export const getSnacks = async(req, res) => {
 
         return res.status(200).send(response)
     } catch (err) {
-        if (err.message === NOT_FOUND) {
-            return res.status(404).send({ Error: 'Not Found' })
-        } else {
             return res.status(400).send({ Error: err.message })
-        }
     }
 }
