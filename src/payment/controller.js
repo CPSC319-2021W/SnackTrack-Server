c8d8fe3ecd50df9ca2ad0d8ee9171c81a015fd44
Hyunuk Lim <dev.hyunuk@gmail.com>
2021-02-17 01:14:44 -0800

    fix: remove unnecessary dataValues
    Co-authored-by: Justin Wong <32123388+justincswong@users.noreply.github.com>

--- src/payment/controller.js
+++ src/payment/controller.js
@@ -12,7 +12,7 @@ export const addPayment = async (req, res) => {
     const payment = req.body
     const result = await Payments.create(payment)
     const transactions = req.body.transaction_ids
-    const paymentId = result.dataValues.payment_id
+    const paymentId = result.payment_id
     transactions.forEach(async id => {
         const transaction = await Transactions.findByPk(id)
         transaction.update({ payment_id: paymentId })
