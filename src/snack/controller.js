c196a0632adf43b1bc8afd5c607c3ebc48d27a0a
HowardBian <77859122+HowardBian@users.noreply.github.com>
2021-02-16 16:25:20 -0800

    SNAK-51 Implement GET /api/v1/snacks endpoint (#11)
    * set up model for snack
    
    * changed db model to be underscored snake case
    
    * first implementation of controller and index for post and get
    
    * got rid of all the ";"
    
    * feat: Implement POST /snacks and GET /snacks
    
    * refactor: POST /snacks adjustments
    
    * feat: implemented GET /snacks
    
    * fix: ensure listing all snacks in database works correctly
    
    * refactor: removed the unnecessary error code check
    
    * refactor: removed the unnecessary allowNull checks
    
    * doc: added TODO for creating snack_batches
    
    * refactor: changed response format
    
    * doc: changed the TODO ticket to better reflect the task
    
    * fix: the response key type is now fixed

--- src/snack/controller.js
+++ src/snack/controller.js
@@ -23,3 +23,15 @@ export const addSnack = async(req, res) => {
         }
     }
 }
+
+// TODO: Implement snack_batches for GET /snacks (SNAK-121)
+export const getSnacks = async(req, res) => {
+    try {
+        const snacks =  await Snacks.findAll()
+        const response = {'snacks': snacks}
+
+        return res.status(200).send(response)
+    } catch (err) {
+            return res.status(400).send({ Error: err.message })
+    }
+}
