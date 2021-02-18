1d977bbeab5a2d9a5487a2284e8dd59e274a002b
bzh <howardbian623@gmail.com>
2021-02-17 22:13:39 -0800

    feat: added POST/ snack_batches

--- src/snack/controller.js
+++ src/snack/controller.js
@@ -1,4 +1,5 @@
 import Snacks from './model.js'
+import SnackBatches from './snackBatches.js'
 
 const BAD_REQUEST = '400'
 const NOT_AUTHORIZED = '401'
@@ -18,6 +19,23 @@ export const addSnack = async(req, res) => {
             return res.status(401).send({ Error: 'Not Authorized' })
         } else if (err.message === CONFLICT) {
             return res.status(409).send({ Error: 'This snack already exists.' })
+        } else {
+            return res.status(500).send({ Error: err.message })
+        }
+    }
+}
+
+export const addSnackBatches = async(req, res) => {
+    try {
+        const snackBatch = req.body
+        const result = await SnackBatches.create(snackBatch)
+
+        return res.status(201).send(result)
+    } catch (err) {
+        if (err.message === BAD_REQUEST) {
+            return res.status(400).send({ Error: 'Bad Request' })
+        } else if (err.message === NOT_AUTHORIZED) { // TODO: wait for authentication to be implemented
+            return res.status(401).send({ Error: 'Not Authorized' })
         } else {
             return res.status(500).send({ Error: err.message })
         }
