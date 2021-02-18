fd2d6f0ec79f47f0decb177bc618e07db1527366
Hyunuk Lim <dev.hyunuk@gmail.com>
2021-02-18 13:48:04 -0800

    fix: make snake_case for fields in endpoints related to transactions and payments (#34)

--- src/util/pagination.js
+++ src/util/pagination.js
@@ -3,11 +3,12 @@ export const getPagination = (page, size) => {
   const offset = page ? page * limit : 0
   return { limit, offset }
 }
-  
+
+// this function's variables and kind, a parameter, must be the snake_case since they will send to the front-end.
 export const getPagingData = (data, page, limit, kind) => {
-  const { count: totalRows, rows } = data
-  const currentPage = page ? +page : 0
-  let totalPages = Math.ceil(totalRows / limit)
-  if (totalPages === 0) totalPages = 1
-  return { totalRows, [kind]: rows, totalPages, currentPage }
+  const { count: total_rows, rows } = data
+  const current_page = page ? +page : 0
+  let total_pages = Math.ceil(total_rows / limit)
+  if (total_pages === 0) total_pages = 1
+  return { total_rows, [kind]: rows, total_pages, current_page }
 }
