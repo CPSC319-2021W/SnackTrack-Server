70f456dd498b9b7c8942f70c65f21308972fd3b8
Hyunuk <dev.hyunuk@gmail.com>
2021-02-25 01:39:06 -0800

    fix: move user's transactions to transactions and some refactoring

--- src/util/pagination.js
+++ src/util/pagination.js
@@ -1,3 +1,6 @@
+const DEFAULT_SIZE = 8
+const DEFAULT_PAGE_OFFSET = 0
+
 export const getPagination = (page, size) => {
   const limit = size ? +size : 8
   const offset = page ? page * limit : 0
@@ -11,4 +14,21 @@ export const getPagingData = (data, page, limit, kind) => {
   let total_pages = Math.ceil(total_rows / limit)
   if (total_pages === 0) total_pages = 1
   return { total_rows, [kind]: rows, total_pages, current_page }
+}
+
+export const getPaginatedData = async (query, where, model, sortKey) => {
+  const { page, size } = query
+  const limit = size ? +size : DEFAULT_SIZE
+  const offset = page ? page * limit : DEFAULT_PAGE_OFFSET
+  const data = await model.findAndCountAll({
+    where,
+    limit,
+    offset,
+    order: [[sortKey, 'DESC']]
+  })
+  const { count: total_rows, rows } = data
+  const current_page = page ? +page : 0
+  let total_pages = Math.ceil(total_rows / limit)
+  if (total_pages === 0) total_pages = 1
+  return { total_rows, [model.name]: rows, total_pages, current_page }
 }
