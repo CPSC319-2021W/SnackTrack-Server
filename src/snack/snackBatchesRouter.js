81a841c591cfab449519681731be8ec3aefeeee5
bzh <howardbian623@gmail.com>
2021-03-04 12:37:45 -0800

    style: added spaces for a more readable code

--- src/snack/snackBatchesRouter.js
+++ src/snack/snackBatchesRouter.js
@@ -1,5 +1,5 @@
 import { Router } from 'express'
-import {addSnackBatches, getSnackBatches} from './controller.js'
+import { addSnackBatches, getSnackBatches } from './controller.js'
 
 const router = Router()
 
