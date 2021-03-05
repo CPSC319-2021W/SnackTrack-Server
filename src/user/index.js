e1691a50734a8da57be3dff578d605cc966e2441
Hyunuk Lim <dev.hyunuk@gmail.com>
2021-03-05 14:49:26 -0800

    feat: implemented GET pendingOrders (#61)

--- src/user/index.js
+++ src/user/index.js
@@ -1,6 +1,6 @@
 import { Router } from 'express'
 import { addUser, getUser } from './controller.js'
-import { getUserTransaction, getUserTransactions } from '../transaction/controller.js'
+import { getUserTransaction, getUserTransactions, getPendingOrders } from '../transaction/controller.js'
 import { getUserPayments } from '../payment/controller.js'
 import { checkPermission } from '../auth/controller.js'
 
@@ -11,5 +11,6 @@ router.get('/:userId', checkPermission, getUser)
 router.get('/:userId/transactions', checkPermission, getUserTransactions)
 router.get('/:userId/transactions/:transactionId', checkPermission, getUserTransaction)
 router.get('/:userId/payments/', checkPermission, getUserPayments)
+router.get('/:userId/pendingOrders/', checkPermission, getPendingOrders)
 
 export default router
