05ad999d0d9419687589e5455774e6e272d582d3
Hyunuk Lim <dev.hyunuk@gmail.com>
2021-03-07 10:29:25 -0800

    Snak 273 - Major refactoring and error handling change (#64)
    * fix: change errors in user
    
    * fix: change errors in suggestions
    
    * fix: change errors in transactions and major refactoring
    
    * fix: change errors in snacks and major refactoring
    
    * fix: change errors in auth
    
    * fix: delete deprecated getPayments and fix bug in putSnacks
    
    * fix: add 409 for addSnacks
    
    * fix: change the error message for addSnack error

--- src/user/controller.js
+++ src/user/controller.js
@@ -4,34 +4,30 @@ import { db } from '../db/index.js'
 // https://www.postgresql.org/docs/8.2/errcodes-appendix.html
 // 23505 = UNIQUE_VIOLATION
 const UNIQUE_VIOLATION = '23505'
-const NOT_FOUND = '404'
-
 const Users = db.users 
 
 export const addUser = async (req, res) => {
   try {
     const user = req.body
     const result = await Users.create(user)
-    res.status(201).send(result)
+    return res.status(201).json(result)
   } catch (err) {
     if (err.parent.code === UNIQUE_VIOLATION) {
-      return res.status(409).send({ Error: err.message })
+      return res.status(409).json({ error: 'User is already exist.' })
     }
-    return res.status(400).send({ Error: err.message })
+    return res.status(400).json({ error: 'User was not created!' })
   }
 }
 
-export const getUser = async(req, res) => {
+export const getUser = async (req, res) => {
   try {
-    const userId = req.params.userId
-    // TODO: Optimization (Ticket: SNAK-93)
-    const resultFromDB = await Users.findByPk(userId)
-    if (resultFromDB === null) throw new Error(404)
-    const response = resultFromDB.toJSON()
-
+    const user_id = req.params.user_id
+    const response = await Users.findByPk(user_id)
+    if (!response) {
+      return res.status(404).json({ error: 'user_id does not exist in the users table' })
+    }
     return res.status(200).json(response)
   } catch (err) {
-    if (err.message === NOT_FOUND) return res.status(404).send({ Error: "userid doesn't exist in the users table" })
-    return res.status(500).send({ Error: err.message })
+    return res.status(500).json({ error: err.message })
   }
 }
