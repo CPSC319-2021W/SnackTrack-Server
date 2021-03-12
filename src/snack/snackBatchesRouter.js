d247b94985e449dbabbfa90762ac9301181d6fe6
HowardBian <77859122+HowardBian@users.noreply.github.com>
2021-03-11 23:08:29 -0800

    Snak 179 Delete /api/v1/snackBatches/{snackBatchId} (#70)
    * comment: edit readme mostly followed FE readme :)
    
    * SNAK-54 Implement POST /api/v1/snacks (#8)
    
    * set up model for snack
    
    * changed db model to be underscored snake case
    
    * first implementation of controller and index for post and get
    
    * got rid of all the ";"
    
    * feat: Implement POST /snacks and GET /snacks
    
    * refactor: POST /snacks adjustments
    
    * refactor: deleted irrelevant imports
    
    * Update src/snack/index.js
    
    Co-authored-by: Justin Wong <32123388+justincswong@users.noreply.github.com>
    
    * refactor: to fix the current style. Made minor changes to export and paths
    
    * fix: the Datatype format that causes the app to crash
    
    * fix: ensure that the responses are expected, waiting for authentication
    
    * refactor: removed the unnecessary allowNull checks
    
    * doc: changed error message
    
    * doc: added TODO for creating snack_batches
    
    Co-authored-by: Justin Wong <32123388+justincswong@users.noreply.github.com>
    
    * Revert "SNAK-54 Implement POST /api/v1/snacks (#8)" (#24)
    
    This reverts commit f0f055e29a6a1616befbe0f10caa29d91f3047ac.
    
    * Fix config.yml
    
    * feat: added DELETE/snack_batches
    
    * refactor: edited error messages' consistency
    
    Co-authored-by: Jessica Huh <40282705+jessHuh@users.noreply.github.com>
    Co-authored-by: Justin Wong <32123388+justincswong@users.noreply.github.com>
    Co-authored-by: KenUtsunomiya <ken.suruga.varuhara@gmail.com>

--- src/snack/snackBatchesRouter.js
+++ src/snack/snackBatchesRouter.js
@@ -1,10 +1,11 @@
 import { Router } from 'express'
-import { addSnackBatches, getSnackBatches } from './controller.js'
+import { addSnackBatches, deleteSnackBatches, getSnackBatches } from './controller.js'
 import { authenticateJWT, isAdmin } from '../auth/controller.js'
 
 const router = Router()
 
 router.post('/', authenticateJWT, isAdmin, addSnackBatches)
 router.get('/', getSnackBatches)
+router.delete('/:snack_batch_id', authenticateJWT, isAdmin, deleteSnackBatches)
 
 export default router
