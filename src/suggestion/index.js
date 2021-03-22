1b6f846cd1b0df839053f5cd56959a544340e3d4
Hyunuk <dev.hyunuk@gmail.com>
2021-03-22 00:23:11 -0700

    fix: implement delete suggestion

--- src/suggestion/index.js
+++ src/suggestion/index.js
@@ -1,10 +1,11 @@
 import { Router } from 'express'
-import { addSuggestion, getSuggestions } from './controller.js'
+import { addSuggestion, getSuggestions, deleteSuggestions } from './controller.js'
 import { authenticateJWT, isAdmin } from '../auth/controller.js'
 
 const router = Router()
 
 router.post('/', addSuggestion)
 router.get('/', authenticateJWT, isAdmin, getSuggestions)
+router.delete('/', authenticateJWT, isAdmin, deleteSuggestions)
 
 export default router
