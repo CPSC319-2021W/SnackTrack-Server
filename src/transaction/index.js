e54bde9829c6cec764c5f85eb20004bb41a254a2
Hyunuk <dev.hyunuk@gmail.com>
2021-03-23 17:44:59 -0700

    feat: GET transactions

--- src/transaction/index.js
+++ src/transaction/index.js
@@ -1,10 +1,11 @@
 import { Router } from 'express'
-import { addTransaction, updateTransaction } from './controller.js'
+import { addTransaction, updateTransaction, getPopularSnacks } from './controller.js'
 import { authenticateJWT, isAdmin } from '../auth/controller.js'
 
 const router = Router()
 
 router.put('/:transaction_id', authenticateJWT, isAdmin, updateTransaction)
 router.post('/', addTransaction)
+router.get('/', getPopularSnacks)
 
 export default router
