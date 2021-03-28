115e3c7dc08b802e3dc89a279ddc33fa1ab86597
KenUtsunomiya <ken.suruga.varuhara@gmail.com>
2021-03-28 02:56:39 -0700

    feat: add a unit test for DELETE /suggestions

--- test/UnitTest/suggestions/suggestions.test.js
+++ test/UnitTest/suggestions/suggestions.test.js
@@ -1,5 +1,5 @@
-import { db } from '../../src/db/index.js'
-import { getSuggestions } from '../../src/suggestion/controller.js'
+import { db } from '../../../src/db/index.js'
+import { deleteSuggestions, getSuggestions } from '../../../src/suggestion/controller.js'
 
 const Suggestions = db.suggestions
 
@@ -54,5 +54,30 @@ describe ('GET /suggestions', () => {
     await getSuggestions(req, res)
     expect(res.status).toHaveBeenCalledWith(200)
     expect(res.json).toHaveBeenCalledWith(expected)
+  })
+})
+
+describe ('DELETE /suggestions', () => {
+  beforeAll (async () => {
+    jest.spyOn(Suggestions, 'destroy').mockImplementation(() => {
+      return Promise.resolve()
+    })
+  })
+
+  it ('should delete suggestions', async () => {
+    const mockRequest = () => {
+      return {}
+    }
+    const mockResponse = () => {
+      const res = {}
+      res.status = jest.fn().mockReturnValue(res)
+      res.json = jest.fn().mockReturnValue(res)
+      return res
+    }
+    const req = mockRequest()
+    const res = mockResponse()
+    await deleteSuggestions(req, res)
+    expect(res.status).toHaveBeenCalledWith(204)
+    expect(res.json).toHaveBeenCalledWith()
   })
 })
