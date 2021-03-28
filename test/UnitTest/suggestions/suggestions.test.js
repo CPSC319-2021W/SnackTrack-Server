7a9b517d27c59a1a71343ead2814ad2a2cb9aa89
KenUtsunomiya <ken.suruga.varuhara@gmail.com>
2021-03-28 03:54:53 -0700

    feat: add unit tests for POST /suggestions

--- test/UnitTest/suggestions/suggestions.test.js
+++ test/UnitTest/suggestions/suggestions.test.js
@@ -1,7 +1,8 @@
 import { db } from '../../../src/db/index.js'
-import { deleteSuggestions, getSuggestions } from '../../../src/suggestion/controller.js'
+import { addSuggestion, deleteSuggestions, getSuggestions } from '../../../src/suggestion/controller.js'
 
 const Suggestions = db.suggestions
+const Users = db.users
 
 describe ('GET /suggestions', () => {
   beforeAll (async () => {
@@ -22,6 +23,8 @@ describe ('GET /suggestions', () => {
       ])
     })
   })
+
+  afterAll (() => jest.clearAllMocks())
 
   it ('should get suggestions', async () => {
     const mockRequest = () => {
@@ -57,12 +60,141 @@ describe ('GET /suggestions', () => {
   })
 })
 
+describe ('POST /suggestions', () => {
+  beforeEach (async () => {
+    jest.spyOn(Users, 'findByPk').mockImplementation((user_id) => {
+      if (user_id === 1) {
+        return Promise.resolve({
+          'user_id': user_id
+        })
+      } else {
+        return Promise.resolve(null)
+      }
+    })
+    jest.spyOn(Suggestions, 'create').mockImplementation((suggestion) => {
+      return Promise.resolve({
+        'suggestion_id': 1,
+        'suggested_by': suggestion.suggested_by,
+        'suggestion_text': suggestion.suggestion_text,
+        'suggestion_dtm': '2012-03-19T07:22Z'
+      })
+    })
+  })
+
+  afterEach (async () => {
+    jest.restoreAllMocks()
+  })
+
+  it ('should create a new suggestion', async () => {
+    const mockRequest = () => {
+      const req = {
+        'body': {
+          'suggested_by': 1,
+          'suggestion_text': 'pocky'
+        }
+      }
+      return req
+    }
+    const mockResponse = () => {
+      const res = {}
+      res.status = jest.fn().mockReturnValue(res)
+      res.json = jest.fn().mockReturnValue(res)
+      return res
+    }
+    const req = mockRequest()
+    const res = mockResponse()
+    const expected = {
+      'suggestion_id': 1,
+      'suggested_by': 1,
+      'suggestion_text': 'pocky',
+      'suggestion_dtm': '2012-03-19T07:22Z'
+    }
+    await addSuggestion(req, res)
+    expect(res.status).toHaveBeenCalledWith(201)
+    expect(res.json).toHaveBeenCalledWith(expected)
+  })
+
+  it ('should reject with 404 - user_id not found', async () => {
+    const mockRequest = () => {
+      const req = {
+        'body': {
+          'suggested_by': 999,
+          'suggestion_text': 'pocky'
+        }
+      }
+      return req
+    }
+    const mockResponse = () => {
+      const res = {}
+      res.status = jest.fn().mockReturnValue(res)
+      res.json = jest.fn().mockReturnValue(res)
+      return res
+    }
+    const req = mockRequest()
+    const res = mockResponse()
+    const expected = { error: 'userid does not exist in the users table.' }
+    await addSuggestion(req, res)
+    expect(res.status).toHaveBeenCalledWith(404)
+    expect(res.json).toHaveBeenCalledWith(expected)
+  })
+
+  it ('should reject with 400 - suggestion text is invalid', async () => {
+    const mockRequest = () => {
+      const req = {
+        'body': {
+          'suggested_by': 1,
+          'suggestion_text': ''
+        }
+      }
+      return req
+    }
+    const mockResponse = () => {
+      const res = {}
+      res.status = jest.fn().mockReturnValue(res)
+      res.json = jest.fn().mockReturnValue(res)
+      return res
+    }
+    const req = mockRequest()
+    const res = mockResponse()
+    const expected = { error: 'suggestionText is empty.' }
+    await addSuggestion(req, res)
+    expect(res.status).toHaveBeenCalledWith(400)
+    expect(res.json).toHaveBeenCalledWith(expected)
+  })
+
+  it ('should reject with 400 - suggestion text is invalid', async () => {
+    const mockRequest = () => {
+      const req = {
+        'body': {
+          'suggested_by': 1,
+          'suggestion_text': '     '
+        }
+      }
+      return req
+    }
+    const mockResponse = () => {
+      const res = {}
+      res.status = jest.fn().mockReturnValue(res)
+      res.json = jest.fn().mockReturnValue(res)
+      return res
+    }
+    const req = mockRequest()
+    const res = mockResponse()
+    const expected = { error: 'suggestionText is empty.' }
+    await addSuggestion(req, res)
+    expect(res.status).toHaveBeenCalledWith(400)
+    expect(res.json).toHaveBeenCalledWith(expected)
+  })
+})
+
 describe ('DELETE /suggestions', () => {
   beforeAll (async () => {
     jest.spyOn(Suggestions, 'destroy').mockImplementation(() => {
       return Promise.resolve()
     })
   })
+
+  afterAll (() => jest.clearAllMocks())
 
   it ('should delete suggestions', async () => {
     const mockRequest = () => {
