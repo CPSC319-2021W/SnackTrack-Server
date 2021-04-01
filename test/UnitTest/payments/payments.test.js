26fd3952828703c51adc529eaafa266d6665b821
KenUtsunomiya <ken.suruga.varuhara@gmail.com>
2021-03-31 21:58:14 -0700

    feat: add unit tests for POST /payments/all

--- test/UnitTest/payments/payments.test.js
+++ test/UnitTest/payments/payments.test.js
@@ -1,5 +1,5 @@
 import { db } from '../../../src/db/index.js'
-import { addPayment, getUserPayments } from '../../../src/payment/controller.js'
+import { addPayment, addPaymentAll, getUserPayments } from '../../../src/payment/controller.js'
 import * as pagination from '../../../src/util/pagination.js'
 
 const Users = db.users
@@ -256,6 +256,136 @@ describe ('POST /payments', () => {
       'user_id': 1
     }
     await addPayment(req, res)
+    expect(res.status).toHaveBeenCalledWith(201)
+    expect(res.json).toHaveBeenCalledWith(expected)
+  })
+})
+
+describe ('POST /payments/all', () => {
+  let req, res
+  beforeAll (async () => {
+    const user1 = {
+      'user_id': 1,
+      'is_admin': true,
+      'username': 'testone',
+      'balance': 1000
+    }
+    jest.spyOn(instance, 'transaction').mockImplementation(() => {
+      const payment = req.body
+      const user_id = payment.user_id
+      const { user_id: currUserId, is_admin } = req.user
+      if (!is_admin && parseInt(user_id) !== currUserId) {
+        return res.status(403).json({ error: 'Not authorized.' })
+      }
+      if (user_id !== 1) {
+        return res.status(404).json({ error: 'user_id does not exist in the users table.' })
+      }
+      payment.payment_amount = user1.balance
+      const result = {
+        'payment_id': 1,
+        'payment_amount': payment.payment_amount,
+        'payment_dtm': '2012-03-19T07:22Z',
+        'created_by': payment.created_by,
+        'user_id': payment.user_id
+      }
+      return Promise.resolve(result)
+    })
+  })
+
+  afterAll (async () => jest.clearAllMocks())
+
+  it ('should reject with 403 - not authorized', async () => {
+    const mockRequest = () => {
+      const req = {
+        'user': {
+          'user_id': 1,
+          'is_admin': false
+        },
+        'body': {
+          'user_id': 2,
+          'created_by': 'test1',
+          'payment_amount': null,
+          'transaction_ids': []
+        }
+      }
+      return req
+    }
+    const mockResponse = () => {
+      const res = {}
+      res.status = jest.fn().mockReturnValue(res)
+      res.json = jest.fn().mockReturnValue(res)
+      return res
+    }
+    req = mockRequest()
+    res = mockResponse()
+    const expected = { error: 'Not authorized.' }
+    await addPaymentAll(req, res)
+    expect(res.status).toHaveBeenCalledWith(403)
+    expect(res.json).toHaveBeenCalledWith(expected)
+  })
+
+  it ('should reject with 404 - user_id not found', async () => {
+    const mockRequest = () => {
+      const req = {
+        'user': {
+          'user_id': 999,
+          'is_admin': false
+        },
+        'body': {
+          'user_id': 999,
+          'created_by': 'test1',
+          'payment_amount': null,
+          'transaction_ids': []
+        }
+      }
+      return req
+    }
+    const mockResponse = () => {
+      const res = {}
+      res.status = jest.fn().mockReturnValue(res)
+      res.json = jest.fn().mockReturnValue(res)
+      return res
+    }
+    req = mockRequest()
+    res = mockResponse()
+    const expected = { error: 'user_id does not exist in the users table.' }
+    await addPaymentAll(req, res)
+    expect(res.status).toHaveBeenCalledWith(404)
+    expect(res.json).toHaveBeenCalledWith(expected)
+  })
+
+  it ('should create a new payment', async () => {
+    const mockRequest = () => {
+      const req = {
+        'user': {
+          'user_id': 1,
+          'is_admin': true
+        },
+        'body': {
+          'user_id': 1,
+          'created_by': 'test1',
+          'payment_amount': null,
+          'transaction_ids': []
+        }
+      }
+      return req
+    }
+    const mockResponse = () => {
+      const res = {}
+      res.status = jest.fn().mockReturnValue(res)
+      res.json = jest.fn().mockReturnValue(res)
+      return res
+    }
+    req = mockRequest()
+    res = mockResponse()
+    const expected = {
+      'payment_id': 1,
+      'payment_amount': 1000,
+      'payment_dtm': '2012-03-19T07:22Z',
+      'created_by': 'test1',
+      'user_id': 1
+    }
+    await addPaymentAll(req, res)
     expect(res.status).toHaveBeenCalledWith(201)
     expect(res.json).toHaveBeenCalledWith(expected)
   })
