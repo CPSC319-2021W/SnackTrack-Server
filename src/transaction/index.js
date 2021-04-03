828b18f165b47bf75c14dab9202827550d84e989
KenUtsunomiya <ken.suruga.varuhara@gmail.com>
2021-04-02 22:44:18 -0700

    fix: fix PUT transactions routes

--- src/transaction/index.js
+++ src/transaction/index.js
@@ -1,10 +1,10 @@
 import { Router } from 'express'
 import { addTransaction, updateTransaction, getPopularSnacks } from './controller.js'
-import { authenticateJWT, isAdmin } from '../auth/controller.js'
+import { authenticateJWT, checkPermission } from '../auth/controller.js'
 
 const router = Router()
 
-router.put('/:transaction_id', authenticateJWT, isAdmin, updateTransaction)
+router.put('/:transaction_id', authenticateJWT, checkPermission, updateTransaction)
 router.post('/', addTransaction)
 router.get('/', getPopularSnacks)
 
